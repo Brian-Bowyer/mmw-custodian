515b0ba13595d32afc3ffd889442678c3fa886e7
Brian-Bowyer <bobsnake64@gmail.com>
2023-12-31 18:41:04 -0600

    on_ready and run

--- bot/bot.py
+++ bot/bot.py
@@ -1,8 +1,18 @@
 import discord
+from constants import SECRET_TOKEN, TESTING_SERVERS
 
 bot = discord.Bot()
 
 
-@bot.command(description="Pings the bot")
+@bot.event
+async def on_ready():
+    print("MMW custodian running!")
+
+
+@bot.command(guild_ids=TESTING_SERVERS, description="Pings the bot")
 async def ping(ctx) -> None:
     await ctx.respond("Pong!")
+
+
+if __name__ == "__main__":
+    bot.run(SECRET_TOKEN)
