cc98f228235dae13810c667f9a7473af2a734f17
Brian-Bowyer <bobsnake64@gmail.com>
2024-01-08 00:25:30 -0600

    more initiative skeleton

--- app/controllers/initiative.py
+++ app/controllers/initiative.py
@@ -1,8 +1,31 @@
+from app.tables.base import database
+
+
 async def create_initiative():
     """Creates an initiative tracker."""
     pass
 
 
-async def delete_initiative():
+async def delete_initiative(tracker_id: str | int):
     """Deletes an initiative tracker."""
+    pass
+
+
+async def get_initiative(channel_id: str | int | None, tracker_id: str | int | None):
+    """Gets an initiative tracker."""
+    pass
+
+
+async def add_to_initiative(tracker_id: str | int, player_name: str, initiative: int):
+    """Adds a character to an initiative tracker."""
+    pass
+
+
+async def remove_from_initiative(tracker_id: str | int, player_name: str):
+    """Removes a character from an initiative tracker."""
+    pass
+
+
+async def update_initiative(tracker_id: str | int, player_name: str, initiative: int):
+    """Updates an initiative tracker."""
     pass
