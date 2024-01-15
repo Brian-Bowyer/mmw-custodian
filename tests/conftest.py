553b8c1aa4571194b6b2968d1f056b108a251678
Brian-Bowyer <bobsnake64@gmail.com>
2024-01-14 21:27:04 -0600

    get rid of conftest since nothing in here is being used

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,1 @@
-import pytest
-import pytest_asyncio
-from databases import Database
 
-from app.constants import DATABASE_URL
-from app.models import Base, database, engine
-
-
-@pytest.fixture(scope="session", autouse=True)
-def recreate_database():
-    """Recreates the database."""
-    Base.metadata.create_all(engine)
-    yield
-    Base.metadata.drop_all(engine)
