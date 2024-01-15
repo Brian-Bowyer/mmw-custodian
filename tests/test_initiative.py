1bebd107c0bdf167dbc65eb875033f057d90cf31
Brian-Bowyer <bobsnake64@gmail.com>
2024-01-14 21:30:50 -0600

    explanatory comment

--- tests/test_initiative.py
+++ tests/test_initiative.py
@@ -5,10 +5,13 @@ from app.constants import DATABASE_URL
 from app.controllers import initiative
 
 
+# NOTE: doing the inefficient thing and creating a new Database object for each test
+# b/c asyncpg's event loop is created when the Database object is, and so would
+# cause an event loop conflict with pytest_asyncio's every-function loops otherwise
 @pytest.mark.asyncio
 async def test_it_creates_an_initiative():
     # assert db.url == database.url
-    async with Database(DATABASE_URL) as db:
+    async with Database(DATABASE_URL, force_rollback=True) as db:
         await initiative.create_initiative("123456", database=db)
         db_entry = await db.fetch_one(
             "SELECT * FROM initiative_trackers WHERE channel_id = '123456'"
