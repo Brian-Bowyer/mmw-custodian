d3aae949b97c905b6d611269d379e9dbaf738d7c
Brian-Bowyer <bobsnake64@gmail.com>
2024-01-14 23:56:51 -0600

    change skeleton to only operate by channel_id since tracker_id is hard to get without channel_id anyway

--- app/controllers/initiative.py
+++ app/controllers/initiative.py
@@ -22,26 +22,26 @@ async def create_initiative(
     log.info("Initiative tracker created!")
 
 
-async def delete_initiative(tracker_id: str | int):
+async def delete_initiative(channel_id: str | int):
     """Deletes an initiative tracker."""
     pass
 
 
-async def get_initiative(channel_id: str | int | None, tracker_id: str | int | None):
+async def get_initiative(channel_id: str | int):
     """Gets an initiative tracker."""
     pass
 
 
-async def add_to_initiative(tracker_id: str | int, player_name: str, initiative: int):
+async def add_to_initiative(channel_id: str | int, player_name: str, initiative: int):
     """Adds a character to an initiative tracker."""
     pass
 
 
-async def remove_from_initiative(tracker_id: str | int, player_name: str):
+async def remove_from_initiative(channel_id: str | int, player_name: str):
     """Removes a character from an initiative tracker."""
     pass
 
 
-async def update_initiative(tracker_id: str | int, player_name: str, initiative: int):
+async def update_initiative(channel_id: str | int, player_name: str, initiative: int):
     """Updates an initiative tracker."""
     pass
